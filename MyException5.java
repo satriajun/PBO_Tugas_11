67ef09c8629435c45303768d1e3bc077fdecece4
satriajun <114549025+satriajun@users.noreply.github.com>
2023-01-31 21:10:16 +0700

    Create MyException5.java

---
+++ MyException5.java
@@ -0,0 +1,25 @@
+package Pratikum_2;
+
+import java.lang.Exception;
+
+public class MyException5 extends Exception{
+    public static void main(String[] args) {
+        int bil=10;
+        try
+        {
+            System.out.println(bil/0);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Pesan error: ");
+            System.out.println(e.getMessage());
+            System.out.println("Info stack erase");
+            e.printStackTrace();
+            e.printStackTrace(System.out);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Ini menghandle error yang terjadi");
+        }
+    }
+}
