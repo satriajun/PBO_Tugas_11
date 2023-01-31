930afd67c1796ed7cd32497bed6dd4466467eb54
satriajun <114549025+satriajun@users.noreply.github.com>
2023-01-31 21:08:33 +0700

    Create MyException3.java

---
+++ MyException3.java
@@ -0,0 +1,16 @@
+package Pratikum_2;
+import java.lang.Exception;
+
+public class MyException3 extends Exception{
+    public static void main(String[] args) {
+        int bil=10;
+        try
+        {
+            System.out.println(bil/0);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Ini menghandle error yang terjadi");
+        }
+    }
+}
