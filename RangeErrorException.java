315b57122531a7a096da844cc6966f1e79b25809
satriajun <114549025+satriajun@users.noreply.github.com>
2023-01-31 21:12:25 +0700

    Create RangeErrorException.java

---
+++ RangeErrorException.java
@@ -0,0 +1,23 @@
+package Pratikum_2;
+import java.lang.Throwable;
+
+public class RangeErrorException extends Throwable{
+    public RangeErrorException(String s){
+    super(s);
+    }
+    public static void main(String[] args){
+        int position=1;
+        try
+        {
+        if(position>0)
+        {
+        throw new RangeErrorException("Position " +position);
+        }
+        }
+        catch(RangeErrorException e)
+        {
+        System.out.println("Range error: " +e.getMessage());
+        }
+        System.out.println("This is the last program.");
+    }
+}
